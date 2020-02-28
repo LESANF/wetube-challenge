3d6bc3a176a26e2c0c309088466d3d0975be5a22
LESANF <nagong1000@naver.com>
2020-02-28 23:39:51 +0900

    errorPage ING

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -17,8 +17,44 @@ export const movieDetail = async (req, res) => {
 
   const detail = await getMovieById(id);
 
-  res.render("movieDetail", { detail });
+  res.render("movieDetail", { pagetitle: detail.title, detail });
 };
-export const filterMovie = (req, res) => {
-  res.render("filterMovie");
+
+export const filterMovie = async (req, res) => {
+  const checkYear = "year";
+  const checkRating = "rating";
+  const checkUrl = req.url;
+  const year = req.url.split("=")[1];
+  const rating = req.url.split("=")[1];
+
+  const checkYearBoolean = checkUrl.includes(checkYear);
+  const checkRatingBoolean = checkUrl.includes(checkRating);
+
+  const parseYear = parseInt(year);
+  const parseRating = parseFloat(rating);
+
+  if (checkYearBoolean) {
+    if (parseYear < 2021 && Number.isInteger(parseYear)) {
+      console.log(Number.isInteger(parseYear));
+      const movies = await getMovieByMinimumYear(parseYear);
+      res.render("home", {
+        pagetitle: `Searching by year: ${parseYear}`,
+        movies
+      });
+    } else {
+      res.render("errorPage");
+    }
+  } else if (checkRatingBoolean) {
+    if (parseRating <= 10) {
+      const movies = await getMovieByMinimumRating(parseRating);
+      res.render("home", {
+        pagetitle: `Searching by rating: ${parseRating}`,
+        movies
+      });
+    } else {
+      res.render("errorPage");
+    }
+  } else {
+    res.render("errorPage");
+  }
 };
--- localsMiddleware.js
+++ localsMiddleware.js
@@ -1,4 +1,5 @@
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "Challenge";
+  res.locals.pagetitle = "Challenge";
   next();
 };
